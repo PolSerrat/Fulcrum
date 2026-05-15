12c995af0edab2522b5a7b0051777ab07915b679
PolSerrat <114682421+PolSerrat@users.noreply.github.com>
2026-05-15 11:03:55 +0200

    OptionsMenu

---
+++ Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class OptionsMenu : MonoBehaviour
+{
+    public GameObject menuPanel; // drag OptionsMenu panel here
+    void Start()
+    {
+        menuPanel.SetActive(true); // visible at scene start
+    }
+    public void CloseMenu()
+    {
+        menuPanel.SetActive(false);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit pressed");
+    }
+    public void LoadNormal()
+    {
+        SceneManager.LoadScene("Level_Normal");
+    }
+    public void LoadHard()
+    {
+        SceneManager.LoadScene("Level_Hard");
+    }
+}
